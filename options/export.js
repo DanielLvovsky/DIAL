f62a6f6959cb0cc5a8700cb163e78635f9e43691
DanielLvovsky <54752143+DanielLvovsky@users.noreply.github.com>
2020-08-07 13:01:11 -0700

    Add files via upload

---
+++ options/export.js
@@ -0,0 +1,25 @@
+var exportBox = document.querySelector("#export textarea")
+var importBox = document.querySelector("#import textarea")
+var importButton = document.querySelector("#import button")
+
+if (window.top !== window) {
+  document.body.classList.add("iframe");
+}
+
+chrome.storage.sync.get(null, function(r) {
+  exportBox.value = JSON.stringify(r)
+})
+
+importButton.onclick = function() {
+  if (confirm(chrome.i18n.getMessage("importexportOverwriteConfirmation"))) {
+    chrome.storage.sync.set(JSON.parse(importBox.value), function() {
+      alert(chrome.i18n.getMessage("importexportSuccessMessage"))
+      window.top.location.reload()
+    })
+  }
+}
+
+document.querySelector("#backbtn").onclick = function(e) {
+  e.preventDefault()
+  history.back()
+}
